e6ff7397c4e04286d2d02f993f827ff3b671782d
Ekaterina Lechitskaia <bobcat7@live.ru>
2023-07-02 18:47:47 +0200

    refactor: вернуть назад стейт

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import PopupWithForm from "./PopupWithForm";
 import { useForm } from "../hooks/useForm";
 
@@ -9,39 +9,17 @@ function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
     setValues({
       ...values,
       photoname: "",
-      imagelink: ""
+      imagelink: "",
     });
-    // setName("");
-    // setLink("");
+    // eslint-disable-next-line
   }, [isOpen]);
-
-  // const [name, setName] = useState("");
-  // const [link, setLink] = useState("");
-
-  // useEffect(() => {
-  //   setName("");
-  //   setLink("");
-  // }, [isOpen]);
-
-  // function handleChangeName(e) {
-  //   setName(e.target.value);
-  // }
-
-  // function handleChangeLink(e) {
-  //   setLink(e.target.value);
-  // }
 
   function handleSubmit(e) {
     e.preventDefault();
     onAddPlace({
       name: values.photoname,
-      link: values.imagelink
+      link: values.imagelink,
     });
-
-    // onAddPlace({
-    //   name,
-    //   link,
-    // });
   }
 
   return (
@@ -59,14 +37,12 @@ function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
           className="popup__input popup__input_content_title"
           type="text"
           id="title-input"
-          // value={name || ""}
           value={values.photoname || ""}
           name="photoname"
           placeholder="Название"
           required=""
           minLength={2}
           maxLength={30}
-          // onChange={handleChangeName}
           onChange={handleChange}
         />
         <span className="popup__error title-input-error" />
@@ -76,12 +52,10 @@ function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
           className="popup__input popup__input_content_image"
           type="url"
           id="link-input"
-          // value={link || ""}
           value={values.imagelink || ""}
           name="imagelink"
           placeholder="Ссылка на картинку"
           required=""
-          // onChange={handleChangeLink}
           onChange={handleChange}
         />
         <span className="popup__error link-input-error" />
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,49 +1,34 @@
 import React, { useState, useEffect, useContext } from "react";
 import PopupWithForm from "./PopupWithForm";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useForm } from "../hooks/useForm";
 
 function EditProfilePopup({ isOpen, onUpdateUser }) {
   const currentUser = useContext(CurrentUserContext);
-  const { values, handleChange, setValues } = useForm({});
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
-    setValues({
-      ...values,
-      username: currentUser.name,
-      useroccupation: currentUser.about,
-    });
-  }, [currentUser, isOpen, setValues]);
+    setName(currentUser.name);
+    setDescription(currentUser.about);
+  }, [currentUser, isOpen]);
 
-  // const [name, setName] = useState("");
-  // const [description, setDescription] = useState("");
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
 
-  // useEffect(() => {
-  //   setName(currentUser.name);
-  //   setDescription(currentUser.about);
-  // }, [currentUser, isOpen]);
-
-  // function handleChangeName(e) {
-  //   setName(e.target.value);
-  // }
-
-  // function handleChangeDescription(e) {
-  //   setDescription(e.target.value);
-  // }
+  function handleChangeDescription(e) {
+    setDescription(e.target.value);
+  }
 
   function handleSubmit(e) {
     // Запрещаем браузеру переходить по адресу формы
     e.preventDefault();
 
     // Передаём значения управляемых компонентов во внешний обработчик
-    // onUpdateUser({
-    //   name,
-    //   about: description,
-    // });
-
     onUpdateUser({
-      name: values.username,
-      about: values.useroccupation,
+      name,
+      about: description,
     });
   }
 
@@ -62,14 +47,12 @@ function EditProfilePopup({ isOpen, onUpdateUser }) {
           type="text"
           id="name-input"
           name="username"
-          // value={name || ""}
-          value={values.username || ""}
+          value={name || ""}
           placeholder="Имя"
           required=""
           minLength={2}
           maxLength={40}
-          // onChange={handleChangeName}
-          onChange={handleChange}
+          onChange={handleChangeName}
         />
         <span className="popup__error name-input-error" />
       </label>
@@ -79,14 +62,12 @@ function EditProfilePopup({ isOpen, onUpdateUser }) {
           type="text"
           id="occupation-input"
           name="useroccupation"
-          // value={description || ""}
-          value={values.useroccupation || ""}
+          value={description || ""}
           placeholder="О себе"
           required=""
           minLength={2}
           maxLength={200}
-          // onChange={handleChangeDescription}
-          onChange={handleChange}
+          onChange={handleChangeDescription}
         />
         <span className="popup__error occupation-input-error" />
       </label>
